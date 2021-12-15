aab3e7a00dc2d81807414061da62938b8b5d85f5
PBWorks111 <42221002+PBWorks111@users.noreply.github.com>
2021-12-15 11:56:21 -0500

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -2,6 +2,21 @@
 * Excercise 1
 *
 */
+
+
+
+/*
+* Then write a function that changes the text and the color inside the div
+*
+*/
+const colorBox = document.getElementById('color-block');
+const colorName = document.getElementById('color-name');
+colorBox.addEventListener('click', (changeColor));
+
+
+
+
+
 function changeColor(){
 
 //     //Write a condition determine what color it should be changed to
@@ -23,10 +38,16 @@ function changeColor(){
     }
 
 }
+    //   
+    //     //change the background color using JS
+        
+    //     //Change the text of the color using the span id color-name
 
 
-/*
-*
+    
+
+
+
 /*
 * For excercise 2, you need to write an event handler for the button id "convertbtn"
 * on mouse click. For best practice use addEventListener.
